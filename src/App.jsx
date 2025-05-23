1a958c08a7bc99256ef6a58d6fa903811d89c78c
Francisco Gleidson Teixeira Rocha <Francisco.Rocha@digitalcollege.internal>
2025-05-23 11:54:08 -0300

    tela de login e LoginContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+import LoginProvider from "./contexts/LoginContext";
 import Rotas from "./routes/Rotas";
 
 
 const App = () => {
   return (
     <>
-      <Rotas />
+      <LoginProvider>
+        <Rotas />
+      </LoginProvider>
+      
     </>
   );
 }
